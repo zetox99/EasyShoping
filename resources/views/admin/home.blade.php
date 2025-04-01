6c9727291c5835fc905ef40e21555801eb615664
ZOTOP7X <root@gmail.com>
2025-04-01 16:43:19 +0100

    create an authentication Admin & User Login System using Laravel

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+   
+</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+  <h1>admin dashboard hiya haddi !</h1>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+ Route::get('redirect',[HomeController::class,'redirect']);
